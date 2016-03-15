9d2b68efc6830e72ec554a6b3a6d1c4f37fcbeca
Ronando Long <ronando.long@gmail.com>
2016-03-15 11:26:31 -0700

    has background image

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,14 @@ $(function() {
 
   }); // form.submit
 
+  var jumboHeight = $('.jumbotron').outerHeight();
+  function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+  }
+
+  $(window).scroll(function(e){
+      parallax();
+  });
+
 });
